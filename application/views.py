848d2db12a65c00e891be7b9e6b77b49ae4940cb
Simvader <simbadfernandes@gmail.com>
2020-08-21 16:59:27 -0400

    base methods http added to view

--- application/views.py
+++ application/views.py
@@ -1,9 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from .models import (
     Drug,
     Vaccination,
 )
+from django.http import Http404
 from .serializers import (
     DrugSerializer,
     VaccinationSerializer
@@ -20,6 +22,41 @@ class DrugView(APIView):
         )
         return Response(serializer.data)
 
+    def post(self, request):
+        serializer = DrugSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors)
+
+
+class DrugRudView(APIView):
+
+    def get_object(self, pk):
+        try:
+            Drug.objects.get(pk=pk)
+        except Drug.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        drug = self.get_object(pk)
+        serializer = DrugSerializer(drug)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        drug = self.get_object(pk)
+        serializer = DrugSerializer(drug, data=request.data)
+        if serialiser.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+    def delete(self, request, pk):
+        drug = self.get_object(pk)
+        drug.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 class VaccinationView(APIView):
 
@@ -30,3 +67,39 @@ class VaccinationView(APIView):
             many=True
         )
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = VaccinationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors)
+
+
+class VaccinationRudView(APIView):
+
+    def get_object(self, pk):
+        try:
+            Vaccination.objects.get(pk=pk)
+        except Vaccination.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        vaccination = self.get_object(pk)
+        serializer = VaccinationSerializer(vaccination)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        vaccination = self.get_object(pk)
+        serializer = VaccinationSerializer(vaccination, data=request.data)
+        if serialiser.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+    def delete(self, request, pk):
+        vaccination = self.get_object(pk)
+        vaccination.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
