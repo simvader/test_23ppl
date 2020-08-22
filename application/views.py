0676a69898279c4ff8c4dc41540850bd306a0729
Simvader <simbadfernandes@gmail.com>
2020-08-21 21:22:44 -0400

    routes for rud views and token view added

--- application/views.py
+++ application/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.authtoken.models import Token
 from .models import (
     Drug,
     Vaccination,
@@ -103,3 +104,12 @@ class VaccinationRudView(APIView):
         vaccination = self.get_object(pk)
         vaccination.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class TokenView(APIView):
+
+    def get(self, request):
+        token = Token.objects.create(user='generic_dummy')
+        return Response({
+            'token': token
+        })
